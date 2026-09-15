021f35440310acfe96c13c3859ca2f2019cc725b
jhkk1220 <jhkk1220@gmail.com>
2026-09-15 23:27:02 +0900

    Create page.js

---
+++ app/oncheonjang/page.js
@@ -0,0 +1,10 @@
+import BranchLanding from "../../components/BranchLanding";
+
+export const dynamic = "force-dynamic";
+
+export const metadata = { title: "쌈보보쌈 온천장점" };
+
+export default function Page() {
+  return <BranchLanding branch="oncheonjang" />;
+}
+
