a49ebfe64ec062c34fb58813379af1826b0f62ca
jhkk1220 <jhkk1220@gmail.com>
2026-09-15 22:26:24 +0900

    Delete adminGuard.js

--- adminGuard.js
+++
@@ -1,7 +0,0 @@
-import { cookies } from "next/headers";
-import { ADMIN_COOKIE_NAME, verifySessionCookieValue } from "./auth";
-
-export function isAdminAuthed() {
-  const c = cookies().get(ADMIN_COOKIE_NAME);
-  return verifySessionCookieValue(c?.value);
-}
